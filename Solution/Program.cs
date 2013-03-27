52b36528d622812e225c5c1261df0ac65d333b47
David Hartveld <david@hartveld.com>
2013-03-27 16:39:02 +0100

    Ex. 5.4 implemented.

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -22,6 +22,17 @@ namespace Solution
 
             var input = (from evt in Observable.FromEventPattern(txt, "TextChanged")
                          select ((TextBox)evt.Sender).Text)
+
+                        .Timestamp()
+                        .Do(inp => Console.WriteLine("I: " + inp.Timestamp.Millisecond + " - " + inp.Value))
+                        .Select(x => x.Value)
+
+                        .Throttle(TimeSpan.FromSeconds(1))
+
+                        .Timestamp()
+                        .Do(inp => Console.WriteLine("T: " + inp.Timestamp.Millisecond + " - " + inp.Value))
+                        .Select(x => x.Value)
+
                         .DistinctUntilChanged();
 
             using (input.Subscribe(inp => Console.WriteLine("User wrote: " + inp)))
