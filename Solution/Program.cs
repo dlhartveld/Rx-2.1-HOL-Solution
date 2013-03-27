750016b6c048f9477bbee24225244a867f875a1b
David Hartveld <david@hartveld.com>
2013-03-27 13:05:15 +0100

    Built exercise 1.

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace Solution
     {
         static void Main(string[] args)
         {
+            IObservable<int> source;
+            IObserver<int> handler;
+
+            IDisposable subscription = source.Select(i -> i + 1).Subscribe(
+                el => Console.WriteLine("Received {0} from source.", el),
+                ex => Console.WriteLine("Source signaled an error: {0}.", ex.Message),
+                () => Console.WriteLine("Source said there are no messages to follow anymore.")
+            );
+
+            Console.WriteLine("Press ENTER to unsubscribe...");
+            Console.ReadLine();
+
+            subscription.Dispose();
         }
     }
 }
